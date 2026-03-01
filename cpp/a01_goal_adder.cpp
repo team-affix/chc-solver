36c2af48aa578fd4421e15ac2cc2082ababd5903
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2026-03-01 09:58:42 -0800

    Added a01_goal_adder

---
+++ cpp/a01_goal_adder.cpp
@@ -0,0 +1,18 @@
+#include "../hpp/a01_goal_adder.hpp"
+
+a01_goal_adder::a01_goal_adder(
+    std::map<const goal_lineage*, const expr*>& g,
+    std::multimap<const goal_lineage*, size_t>& c,
+    const std::vector<const rule*>& d)
+    : goals(g), candidates(c), database(d)
+{
+}
+
+void a01_goal_adder::operator()(const goal_lineage* l, const expr* e)
+{
+    goals.insert({l, e});
+
+    // add EVERY rule in the database as a candidate for the goal (trivial)
+    for (size_t i = 0; i < database.size(); ++i)
+        candidates.insert({l, i});
+}
