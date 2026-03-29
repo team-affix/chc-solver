ec6dd1d5ee6832deca24146a3fe40af5edd1dfd8
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2026-03-28 23:40:40 -0700

    Implemented the horizon sim as inheriting from ridge_sim

--- cpp/horizon_sim.cpp
+++ cpp/horizon_sim.cpp
@@ -1,84 +1,26 @@
-// #include "../hpp/horizon_sim.hpp"
+#include "../hpp/horizon_sim.hpp"
 
-// horizon_sim::horizon_sim(
-//     size_t max_resolutions,
-//     const database& db,
-//     const goals& goals,
-//     trail& t,
-//     sequencer& seq,
-//     expr_pool& ep,
-//     bind_map& bm,
-//     lineage_pool& lp,
-//     cdcl c,
-//     monte_carlo::simulation<mcts_decider::choice, std::mt19937>& sim
-// ) :
-//     max_resolutions(max_resolutions),
-//     db(db),
-//     t(t),
-//     lp(lp),
-//     rs({}),
-//     ds({}),
-//     cp(seq, ep),
-//     gs(db, goals, t, cp, bm, lp),
-//     cs(db, goals, lp),
-//     ws(goals, db, lp),
-//     dec(cs, sim),
-//     c(c)
-// {}
+horizon_sim::horizon_sim(
+    size_t max_resolutions,
+    const database& db,
+    const goals& goals,
+    trail& t,
+    sequencer& seq,
+    expr_pool& ep,
+    bind_map& bm,
+    lineage_pool& lp,
+    cdcl c,
+    monte_carlo::simulation<mcts_decider::choice, std::mt19937>& mc_sim
+) :
+    ridge_sim(max_resolutions, db, goals, t, seq, ep, bm, lp, c, mc_sim),
+    ws(goals, db, lp)
+{}
 
-// bool horizon_sim::operator()() {
-    
-//     while ((rs.size() < max_resolutions) && !cs.conflicted() && !gs.empty()) {
+double horizon_sim::reward() {
+    return ws.total();
+}
 
-//         // head elimination
-//         size_t elim0 = cs.eliminate([this](const goal_lineage* gl, size_t i) { return !gs.applicable(gs.at(gl), db.at(i)); });
-
-//         // cdcl elimination
-//         size_t elim1 = cs.eliminate([this](const goal_lineage* gl, size_t i) { return c.eliminated(lp.resolution(gl, i)); });
-        
-//         // unit propagation
-//         const goal_lineage* propagated_gl;
-//         size_t propagated_rule_id;
-//         bool unit = cs.unit(propagated_gl, propagated_rule_id);
-
-//         // if a unit propagation is found, enact it
-//         if (unit)
-//             resolve(lp.resolution(propagated_gl, propagated_rule_id));
-
-//         // continue until fixpoint
-//         if (elim0 > 0 || elim1 > 0 || unit)
-//             continue;
-
-//         // decide on a goal and candidate
-//         auto [chosen_goal, chosen_candidate] = dec();
-
-//         // construct the resolution lineage
-//         const resolution_lineage* rl = lp.resolution(chosen_goal, chosen_candidate);
-
-//         // resolve the chosen goal and candidate
-//         resolve(rl);
-        
-//         // mark this resolution as a decision
-//         ds.insert(rl);
-
-//     }
-
-//     // return whether a solution was found
-//     return gs.empty();
-// }
-
-// const resolutions& horizon_sim::get_resolutions() const {
-//     return rs;
-// }
-
-// const decisions& horizon_sim::get_decisions() const {
-//     return ds;
-// }
-
-// void horizon_sim::resolve(const resolution_lineage* rl) {
-//     gs.resolve(rl);
-//     cs.resolve(rl);
-//     ws.resolve(rl);
-//     c.constrain(rl);
-//     rs.insert(rl);
-// }
+void horizon_sim::on_resolve(const resolution_lineage* rl) {
+    ridge_sim::on_resolve(rl);
+    ws.resolve(rl);
+}
--- hpp/horizon_sim.hpp
+++ hpp/horizon_sim.hpp
@@ -1,55 +1,29 @@
-// #ifndef HORIZON_SIM_HPP
-// #define HORIZON_SIM_HPP
+#ifndef HORIZON_SIM_HPP
+#define HORIZON_SIM_HPP
 
-// #include "../mcts/include/mcts.hpp"
-// #include "defs.hpp"
-// #include "sequencer.hpp"
-// #include "bind_map.hpp"
-// #include "goal_store.hpp"
-// #include "candidate_store.hpp"
-// #include "weight_store.hpp"
-// #include "mcts_decider.hpp"
-// #include "cdcl.hpp"
+#include "ridge_sim.hpp"
+#include "weight_store.hpp"
 
-// struct horizon_sim {
-//     horizon_sim(
-//         size_t,
-//         const database&,
-//         const goals&,
-//         trail&,
-//         sequencer&,
-//         expr_pool&,
-//         bind_map&,
-//         lineage_pool&,
-//         cdcl c,
-//         monte_carlo::simulation<mcts_decider::choice, std::mt19937>&
-//     );
-//     bool operator()();
-//     const resolutions& get_resolutions() const;
-//     const decisions& get_decisions() const;
-// #ifndef DEBUG
-// private:
-// #endif
-//     void resolve(const resolution_lineage*);
+struct horizon_sim : ridge_sim {
+    horizon_sim(
+        size_t,
+        const database&,
+        const goals&,
+        trail&,
+        sequencer&,
+        expr_pool&,
+        bind_map&,
+        lineage_pool&,
+        cdcl c,
+        monte_carlo::simulation<mcts_decider::choice, std::mt19937>&
+    );
+    double reward();
+#ifndef DEBUG
+private:
+#endif
+    void on_resolve(const resolution_lineage*) override;
 
-//     size_t max_resolutions;
+    weight_store ws;
+};
 
-//     const database& db;
-//     trail& t;
-//     lineage_pool& lp;
-    
-//     resolutions rs;
-//     decisions ds;
-    
-//     copier cp;
-
-//     goal_store gs;
-//     candidate_store cs;
-//     weight_store ws;
-    
-//     mcts_decider dec;
-    
-//     cdcl c;
-// };
-
-// #endif
+#endif
