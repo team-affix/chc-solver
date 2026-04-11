13b12c3b3ce60ba57c9d9f13cf72e728ae32d0c4
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2026-04-10 19:25:43 -0700

    Added delta_map type

---
+++ core/hpp/delta_map.hpp
@@ -0,0 +1,85 @@
+#ifndef DELTA_MAP_HPP
+#define DELTA_MAP_HPP
+
+#include <map>
+#include <memory>
+#include <optional>
+#include <utility>
+#include "trail.hpp"
+#include "delta.hpp"
+#include "tracked.hpp"
+
+template<typename K, typename V>
+struct delta<std::map<K, V>> {
+    delta(trail& t, const std::map<K, V>& value) : underlying(t, value) {}
+    void insert(const K& key, const V& value) {
+        // create the key register
+        std::shared_ptr<std::optional<K>> key_reg =
+            std::make_shared<std::optional<K>>(key);
+
+        // create the value register
+        std::shared_ptr<std::optional<V>> value_reg =
+            std::make_shared<std::optional<V>>(value);
+
+        underlying.mutate(
+            [this, key, value, key_reg, value_reg](std::map<K, V>& m) {
+                // take the value from the register
+                m.emplace(std::move(*key_reg), std::move(*value_reg));
+                key_reg.reset();
+                value_reg.reset();
+            },
+            [this, key, value, key_reg, value_reg](std::map<K, V>& m) {
+                // take the value from the map
+                auto node = m.extract(key);
+                key_reg->value() = std::move(node.key());
+                value_reg->value() = std::move(node.value());
+            }
+        );
+    }
+    void assign(const K& key, const V& value) {
+        // create the register for the value
+        std::shared_ptr<V> reg =
+            std::make_shared<V>(value);
+
+        // create the swapper function
+        auto swapper = [this, key, reg](std::map<K, V>& m) {
+            std::swap(m.at(key), *reg);
+        };
+        
+        // the swapper is an involution so fwd and bwd are same
+        underlying.mutate(
+            swapper,
+            swapper
+        );
+    }
+    void erase(const K& key) {
+        // create the key register
+        std::shared_ptr<std::optional<K>> key_reg =
+            std::make_shared<std::optional<K>>(std::nullopt);
+
+        // create the value register
+        std::shared_ptr<std::optional<V>> value_reg =
+            std::make_shared<std::optional<V>>(std::nullopt);
+
+        underlying.mutate(
+            [this, key, key_reg, value_reg](std::map<K, V>& m) {
+                // take the value from the map
+                auto node = m.extract(key);
+                key_reg->value() = std::move(node.key());
+                value_reg->value() = std::move(node.value());
+            },
+            [this, key, key_reg, value_reg](std::map<K, V>& m) {
+                // take the value from the register
+                m.emplace(std::move(*key_reg), std::move(*value_reg));
+                key_reg.reset();
+                value_reg.reset();
+            }
+        );
+    }
+#ifndef DEBUG
+private:
+#endif
+    tracked<std::map<K, V>> underlying;
+};
+
+#endif
