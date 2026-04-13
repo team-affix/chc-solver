96595d1564fa9897d841c3fbf91e62a663fc9710
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2026-04-13 16:35:51 -0700

    Updated tracked type given update to the trail API

--- core/hpp/tracked.hpp
+++ core/hpp/tracked.hpp
@@ -8,7 +8,7 @@ struct tracked {
     tracked(trail& t, const T& value) : t(t), value(value) {}
     void mutate(std::function<void(T&)> fwd, std::function<void(T&)> bwd) {
         fwd(value);
-        t.log([this, bwd]{bwd(value);}, [this, fwd]{fwd(value);});
+        t.log({[this, bwd]{bwd(value);}, [this, fwd]{fwd(value);}});
     }
     const T& get() const { return value; }
 #ifndef DEBUG
