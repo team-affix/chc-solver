ce008f5e6892e037c428c07bb23c34f074c0f92a
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2026-04-25 02:21:51 -0700

    Updated cdcl_eliminator to maintain an active_goals and resolved_goals set to route events properly

--- core/cpp/cdcl_eliminator.cpp
+++ core/cpp/cdcl_eliminator.cpp
@@ -1,25 +1,71 @@
 #include "../hpp/cdcl_eliminator.hpp"
 
-cdcl_eliminator::cdcl_eliminator(cdcl& c, lineage_pool& lp, candidate_store& cs) : c(c), lp(lp), cs(cs) {}
+cdcl_eliminator::cdcl_eliminator(
+    const database& db,
+    const goals& goals,
+    expr_pool& ep,
+    candidate_store& cs,
+    lineage_pool& lp,
+    cdcl& c
+) : lp(lp), cs(cs), fw(db, lp) {
+    fw.set_insert_callback(goal_inserted_callback());
+    fw.set_resolve_callback(goal_resolved_callback());
+    fw.initialize(goals);
+}
 
-void cdcl_eliminator::pipe() {
-    auto& new_eliminated_resolutions = c.new_eliminated_resolutions;
-    
+void cdcl_eliminator::execute() {
     // fill the elimination backlog with the new eliminated resolutions
     while (!new_eliminated_resolutions.empty()) {
         // get the next eliminated resolution
         const resolution_lineage* rl = new_eliminated_resolutions.front();
         new_eliminated_resolutions.pop();
         
-        // push the resolution to the elimination backlog
-        elimination_backlog[rl->parent].insert(rl->idx);
+        // route the elimination properly
+        route_elimination(rl);
     }
 }
-void cdcl_eliminator::execute() {
-    // process the elimination backlog according to the goals in the frontier
-    for (auto& [gl, backlog] : elimination_backlog) {
-        for (size_t idx : backlog)
-            eliminated_frontier_resolutions.push(lp.resolution(gl, idx));
-        backlog.clear();
+
+void cdcl_eliminator::route_elimination(const resolution_lineage* rl) {
+    // get the parent goal
+    const goal_lineage* gl = rl->parent;
+
+    // if the goal is resolved, do nothing
+    if (resolved_goals.contains(gl))
+        return;
+    
+    // if the goal not in the frontier, add to backlog and return
+    if (!active_goals.contains(gl)) {
+        elimination_backlog[gl].insert(rl->idx);
+        return;
     }
+
+    // if the goal is in the frontier, eliminate right now in candidate_store
+    eliminate(gl, rl->idx);
+
+}
+
+void cdcl_eliminator::flush_backlog_for_goal(const goal_lineage* gl) {
+    auto node = elimination_backlog.extract(gl);
+
+    // for each index in the backlog, eliminate the candidate
+    for (size_t idx : node.mapped())
+        eliminate(gl, idx);
+}
+
+void cdcl_eliminator::eliminate(const goal_lineage* gl, size_t idx) {
+    cs.at(gl).erase(idx);
+}
+
+std::function<void(const goal_lineage*)> cdcl_eliminator::goal_inserted_callback() {
+    return [this](const goal_lineage* gl) {
+        active_goals.insert(gl);
+        flush_backlog_for_goal(gl);
+    };
+}
+
+std::function<void(const resolution_lineage*)> cdcl_eliminator::goal_resolved_callback() {
+    return [this](const resolution_lineage* rl) {
+        active_goals.erase(rl->parent);
+        resolved_goals.insert(rl->parent);
+    };
 }
--- core/hpp/cdcl_eliminator.hpp
+++ core/hpp/cdcl_eliminator.hpp
@@ -4,19 +4,35 @@
 #include "cdcl.hpp"
 #include "lineage.hpp"
 #include "candidate_store.hpp"
+#include "frontier_watch.hpp"
+#include <queue>
 
 struct cdcl_eliminator {
-    cdcl_eliminator(cdcl&, lineage_pool&);
-    void add_frontier_goal(const goal_lineage*);
-    void remove_frontier_goal(const goal_lineage*);
-    void pipe();
+    cdcl_eliminator(
+        const database&,
+        const goals&,
+        expr_pool&,
+        candidate_store&,
+        lineage_pool&,
+        cdcl&);
+    void execute();
 #ifndef DEBUG
 private:
 #endif
-    cdcl& c;
+    void route_elimination(const resolution_lineage*);
+    void flush_backlog_for_goal(const goal_lineage*);
+    void eliminate(const goal_lineage*, size_t);
+    
+    std::function<void(const goal_lineage*)> goal_inserted_callback();
+    std::function<void(const resolution_lineage*)> goal_resolved_callback();
+
     lineage_pool& lp;
     candidate_store& cs;
 
+    frontier_watch fw;
+    std::unordered_set<const goal_lineage*> active_goals;
+    std::unordered_set<const goal_lineage*> resolved_goals;
+    std::queue<const resolution_lineage*> new_eliminated_resolutions;
     std::unordered_map<const goal_lineage*, std::unordered_set<size_t>> elimination_backlog;
 };
 
