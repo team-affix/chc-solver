71c839ca2a2fc24eccb93be4321858967d518cf6
teamaffixmb-jake <teamaffixmb.jake@gmail.com>
2026-04-25 01:17:12 -0700

    Updated frontier to supply the actual parent goal_lineage* to make_expander

--- core/hpp/frontier.hpp
+++ core/hpp/frontier.hpp
@@ -14,7 +14,7 @@ struct frontier {
     void insert(const goal_lineage*, const T&);
     void resolve(const resolution_lineage*);
     
-    virtual Expander make_expander(const T&, const rule&) = 0;
+    virtual Expander make_expander(const goal_lineage* const&, const rule&) = 0;
 
     T& at(const goal_lineage*);
     const T& at(const goal_lineage*) const;
@@ -43,14 +43,11 @@ void frontier<T, Expander>::resolve(const resolution_lineage* r) {
     // get the parent
     const goal_lineage* parent = r->parent;
     
-    // get the value of the parent
-    const T& parent_value = members.at(parent);
-
     // get the rule at the resolution index
     const rule& db_rule = db.at(r->idx);
     
     // create the expander for the parent
-    Expander exp{make_expander(parent_value, db_rule)};
+    Expander exp{make_expander(parent, db_rule)};
     
     // add the children to the frontier
     for (int i = 0; i < db_rule.body.size(); ++i)
